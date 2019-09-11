3fb4de4d50c1af1cd24ca7eb2f646a10da61582a
Ruan <ruan_03@hotmail.com>
2019-09-10 21:55:20 -0300

    cadastro usuario - post

--- NodeJs/Modulo02/GoBarber/src/routes.js
+++ NodeJs/Modulo02/GoBarber/src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Ruan Nicolini',
-    email: 'ruannicolini@hotmail.com',
-    password_hash: '123456'
-  });
-  // return res.json({ message: 'Ola mundo doido de meu Deus!' });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 // module.exports = routes;
 export default routes;
