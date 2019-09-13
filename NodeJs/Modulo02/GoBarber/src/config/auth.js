a5ad669472d12af241034c65cb6a75e09097363d
Ruan <ruan_03@hotmail.com>
2019-09-12 22:17:49 -0300

    Autenticação Token JWT

---
+++ NodeJs/Modulo02/GoBarber/src/config/auth.js
@@ -0,0 +1,5 @@
+export default {
+  // Esse numero grande é o md5 que eu gerei no MD5 online com a string'gobarber'. https://www.md5online.org/
+  secret: '8538607221f2e42284acf599214cfa34',
+  expiresIn: '7d'
+};
