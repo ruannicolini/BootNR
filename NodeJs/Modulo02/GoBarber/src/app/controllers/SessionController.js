44dcf5e422fca16802761d1243e628f1fa9d403d
Ruan <ruan_03@hotmail.com>
2019-09-17 19:13:23 -0300

    validacoes com YAP

--- NodeJs/Modulo02/GoBarber/src/app/controllers/SessionController.js
+++ NodeJs/Modulo02/GoBarber/src/app/controllers/SessionController.js
@@ -4,6 +4,17 @@ import authConfig from '../../../src/config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
 
