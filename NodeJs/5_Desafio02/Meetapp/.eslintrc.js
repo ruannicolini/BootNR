e9197b745ee255a7d2c72fca3832ccab714a3a1e
Ruan <ruan_03@hotmail.com>
2019-09-24 22:10:17 -0300

    Config meetapp

--- NodeJs/5_Desafio02/Meetapp/.eslintrc.js
+++ NodeJs/5_Desafio02/Meetapp/.eslintrc.js
@@ -1,21 +1,23 @@
 module.exports = {
   env: {
     es6: true,
-    node: true,
+    node: true
   },
   extends: ['airbnb-base'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
+  // parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
+  plugins: ['@typescript-eslint'],
   rules: {
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     camelcase: 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
-  },
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }]
+  }
 };
