1441dca3273e94c2f42667206b866552c7a7296a
ruannicolini <ruan_03@hotmail.com>
2019-12-16 21:53:47 -0300

    mongo connection add

--- NodeJs/6_Modulo03/6_GoBarber/src/database/index.js
+++ NodeJs/6_Modulo03/6_GoBarber/src/database/index.js
@@ -1,4 +1,6 @@
 import Sequelize, { DatabaseError } from 'sequelize';
+import mongoose from 'mongoose';
+
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -9,12 +11,22 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
+
   init() {
     this.connection = new Sequelize(databaseConfig);
 
     models.map(model => model.init(this.connection));
     models.map(model => model.associate && model.associate(this.connection.models) );
+  }
+
+  mongo(){
+    this.mongoConnection = mongoose.connect(
+      'mongodb://192.168.99.100:27017/gobarber',
+      // { useNewUrlParser: true, useFindAndModify: true }
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
   }
 }
 export default new Database();
