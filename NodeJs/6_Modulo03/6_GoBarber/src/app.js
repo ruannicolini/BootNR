74ad1c951eb880f5f899e4455a2bee4bc9c802e0
ruannicolini <ruan_03@hotmail.com>
2020-01-04 17:29:21 -0300

    Monitoramento de Erros com Sentry

--- NodeJs/6_Modulo03/6_GoBarber/src/app.js
+++ NodeJs/6_Modulo03/6_GoBarber/src/app.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
 import routes from './routes';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
 
 import './database';
+import 'express-async-errors';
+
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+  
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
+
     this.server.use(express.json());
 
     // servir arquivos staticos
@@ -23,7 +34,24 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
+
+  exceptionHandler() {
+
+    this.server.use(async (err, req, res, next) => {
+
+      // if (process.env.NODE_ENV === 'development') {
+        
+        const erros = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(erros);
+      // }
+
+      // return res.status(500).json({ error: 'Internal server error.' });
+    });
+  }
+
 }
 
 // module.exports = new App().server;
---
+++ NodeJs/6_Modulo03/6_GoBarber/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dsn: 'https://a0f1b734e8a045c2982a0995abb683d3@sentry.io/1871755'
+};
