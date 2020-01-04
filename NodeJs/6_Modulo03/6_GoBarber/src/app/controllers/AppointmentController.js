92427d37ddbb57aaff767fc0f0062997b79ec550
ruannicolini <ruan_03@hotmail.com>
2020-01-04 15:20:04 -0300

    Campos virtuais no agendamento

--- NodeJs/6_Modulo03/6_GoBarber/src/app/controllers/AppointmentController.js
+++ NodeJs/6_Modulo03/6_GoBarber/src/app/controllers/AppointmentController.js
@@ -20,7 +20,7 @@ class AppointmentController{
         const appointments = await Appointment.findAll({
             where: { user_id: req.userId, canceled_at: null },
             order: ['date'],
-            attributes : ['id','date'],
+            attributes : ['id','date','past','cancelable'],
             limit: qtdRegPag,
             offset: (page - 1) * qtdRegPag,
 
