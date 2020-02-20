e734e7113c07cafbed7abd4219ab5f549959430d
ruannicolini <ruan_03@hotmail.com>
2020-02-19 21:55:37 -0300

    MeetupController add

---
+++ NodeJs/7_Desafio03/Meetapp/src/app/controllers/MeetupController.js
@@ -0,0 +1,16 @@
+
+class MeetupController {
+    async store(req, res){
+
+        
+
+        return res.json('oi');
+
+    }
+
+    async update(req,res){
+
+    }
+}
+
+export default new MeetupController();
--- NodeJs/7_Desafio03/Meetapp/src/routes.js
+++ NodeJs/7_Desafio03/Meetapp/src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer'
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -22,6 +23,10 @@ routes.post('/session', SessionController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
+routes.post('/meetups',MeetupController.store);
+
 routes.put('/users', authMiddleware, UserController.update);
+
+routes.put('/meetups/:id',MeetupController.update);
 
 export default routes;
