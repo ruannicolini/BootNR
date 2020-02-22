3f2ca35a8d244dd008d7fe6285f09523f41d3821
ruannicolini <ruan_03@hotmail.com>
2020-02-22 16:13:22 -0300

    Meetup Yup Validation add

--- NodeJs/7_Desafio03/Meetapp/src/app/controllers/MeetupController.js
+++ NodeJs/7_Desafio03/Meetapp/src/app/controllers/MeetupController.js
@@ -1,14 +1,35 @@
 
+import * as Yup from 'yup';
+
 class MeetupController {
+
     async store(req, res){
 
-        
+        const schema = Yup.object().shape({
+            title: Yup.string().required(),
+            file_id: Yup.number().required(),
+            user_id: Yup.number().required(),
+            description: Yup.string().required(),
+            location: Yup.string().required(),
+            date: Yup.date().required() //.min( new Date )
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
+        const { user_id, file_id } = req.body;
+
 
         return res.json('oi');
 
     }
 
     async update(req,res){
+
+    }
+
+    async delete(req,res){
 
     }
 }
