75320330dd37d13720eda057b60257bdced0d0c2
ruannicolini <ruan_03@hotmail.com>
2020-07-18 17:09:24 -0300

    primeiro projeto config reactJS

--- NodeJs/8_Modulo04 React/2_modulo04/src/App.js
+++ NodeJs/8_Modulo04 React/2_modulo04/src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./App.css";
 import bgImage from "./assets/background.jpg";
@@ -8,27 +10,39 @@ function App() {
   // useState sempre retorna um array com 2 posicoes.
   // na primeira posicao a variavel com os valores do estado inicial
   // na segunda posicao uma funcao para atualizar os estados
-  const [projects, setProjects] = useState([
-    "Desenvolvimento App",
-    "Front-end Web",
-  ]);
+  const [projects, setProjects] = useState([]);
 
-  function handleAddProject() {
+  //teste
+  useEffect(() => {
+    api.get("patologia").then((response) => {
+      setProjects(response.data);
+      console.log(response);
+    });
+  }, []);
+
+  async function handleAddProject() {
     //setState deve criar sempre um novo registro, nunca alterar o valor anterior.
     // por isso criei um novo array com todas as informacoes do antigo mais o novo projeto
     // Conceito de imultabilidade dos estados no react
-    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+
+    const response = await api.post("patologia", {
+      descricao: `testa add patologia pelo reactJs (${Date.now()})`,
+    });
+
+    const project = response.data;
+    setProjects([...projects, project]);
   }
 
   return (
     <>
       <Header title="projects" />
 
-      <img width={3000} src={bgImage} />
+      {/* <img width={3000} src={bgImage} /> */}
 
       <ul>
         {projects.map((proj) => (
-          <li key={proj}>{proj}</li>
+          <li key={proj.id}>{proj.descricao}</li>
         ))}
       </ul>
 
