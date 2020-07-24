105a38ea827b46eb9770ccaa2b88d3be9701c4f6
ruannicolini <ruan_03@hotmail.com>
2020-07-23 21:08:49 -0300

    estilo detalhe repositorio

--- NodeJs/8_Modulo04 React/ReactJs/3_apiGitHub/api-git-hub/src/pages/Repository/index.tsx
+++ NodeJs/8_Modulo04 React/ReactJs/3_apiGitHub/api-git-hub/src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,55 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/6893004?s=400&u=8ecddc66048cb8a1813e1c480fd04af22ed6a047&v=4"
+            alt="teste"
+          />
+          <div>
+            <strong>ruannicolini/Sogym</strong>
+            <p>descricao do repositorio</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Starts</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertos</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to={''}>
+          <div>
+            <strong>hsafjkdsf</strong>
+            <p>sdfghjkjhg</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
